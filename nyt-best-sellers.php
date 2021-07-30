66f9542048da701ee0c8abe02fba6a169da0b33b
Florian Perrier <florian@lagenceoueb.tech>
2021-07-30 12:32:19 +0200

    Initial commit

---
+++ nyt-best-sellers.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Plugin Name: New York Times - Best Sellers
+ * Description: This plugin will provides you all newest bestsellers.
+ * Version: 1.0
+ * Author: The best team ever
+ */
+
+ 
