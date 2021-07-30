c8a91fa0f26f904edccc68fc00512ee6384501cf
Wendy Afrim <wendy.afrim2@gmail.com>
2021-07-30 15:16:16 +0200

    Création de la table Books à l'activation du plugin Best Sellers

--- nyt-best-sellers.php
+++ nyt-best-sellers.php
@@ -7,4 +7,28 @@
  * Author: The best team ever
  */
 
+function nyt_activate()
+{
+    global $wpdb;
+ 
+    $charset_collate = $wpdb->get_charset_collate();
+ 
+    $tableName = $wpdb->prefix . 'books';
+ 
+    $sql = "CREATE TABLE $tableName (
+        id bigint(20) unsigned NOT NULL auto_increment,
+        title varchar(255) NOT NULL,
+        description varchar(255) NOT NULL,
+        author varchar(255) NOT NULL,
+        price int(20) unsigned NOT NULL,
+        publisher varchar(255) NOT NULL,
+        primary_isbn13 int(20) NOT NULL,
+        primary_isbn10 int(20) NOT NULL,
+        PRIMARY KEY  (id)
+    ) $charset_collate;";
+ 
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
+}
+register_activation_hook(__FILE__, 'nyt_activate');
  
